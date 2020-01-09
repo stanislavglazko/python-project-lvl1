4832828c6ba5e7f9da2362c4ab4ae870948c26ee
stanislavglazko <stanislavglazko@gmail.com>
2020-01-09 20:23:05 +0300

    new version of gcd

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -14,7 +14,7 @@ def make_round():
     random1 = random.randint(1, 100)
     random2 = random.randint(1, 100)
     random_number = str(random1) + " " + str(random2)
-    correct_answer = gcd(random1, random2)
+    correct_answer = str(gcd(random1, random2))
     return random_number, correct_answer
 
 
